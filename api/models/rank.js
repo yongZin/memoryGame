a3a69894ec4f5ba857872ddc6d638dbc1d2ab28b
yongZin-MAC <>
2024-07-29 15:32:51 +0900

    Remove node_modules from git tracking

--- api/models/rank.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-		userName: { type: String, require: true },
-		userRecord: { type: String, require: true }
-	},{ timestamps: true }
-);
-
-module.exports = mongoose.model('rank', UserSchema);
